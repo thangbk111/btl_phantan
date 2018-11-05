b3f6ea965484f4ed25231d826f20fba1382f17df
thang.nv <thang.nv>
2018-11-05 11:15:43 +0700

    fff

--- faker.js
+++ faker.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const faker = require('faker');
+var User = require('../models/user');
+
+const maxRecord = 10;
 
 router.get('/', (req, res) => {
     //Fake User
-    
+    for (let i = 0; i < 10; i++) {
+	    User.create({
+	        name: faker.fake("{{name.lastName}} {{name.firstName}}"),
+	        email: req.body.email,
+	        password: req.body.password
+	    });
+    }
 });
 
 module.exports = router;
