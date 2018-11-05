bbcfcc55229e2634640db22cb24c98e67f97ce24
thang.nv <thang.nv>
2018-11-05 13:16:32 +0700

    faker User and Meeting Table

--- app.js
+++ app.js
@@ -2,10 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const morgan = require('morgan');
-var users = require('./routes/users');
-var meetings = require('./routes/meetings');
-var roles = require('./routes/roles');
-var subContents = require('./routes/sub_contents');
 var faker = require('./faker');
 
 const app = express();
@@ -19,9 +15,6 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Route
-app.use('/api/users', users);
-app.use('/api/meetings', meetings);
-app.use('/api/text_processing', subContents);
 app.use('/faker', faker);
 
 app.listen(app.get('port'), () => console.log(`Listing to port ${app.get('port')}`));
--- faker.js
+++ faker.js
@@ -1,19 +1,59 @@
 const express = require('express');
 const router = express.Router();
 const faker = require('faker');
-var User = require('../models/user');
+var User = require('./models/user');
+var Meeting = require('./models/meeting');
 
 const maxRecord = 10;
 
-router.get('/', (req, res) => {
+router.get('/all', (req, res) => {
     //Fake User
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < maxRecord; i++) {
 	    User.create({
 	        name: faker.fake("{{name.lastName}} {{name.firstName}}"),
-	        email: req.body.email,
-	        password: req.body.password
+	        email: faker.internet.email(),
+	        password: '1234'
 	    });
-    }
+	}
+
+    for (let i = 0; i < maxRecord; i++) {
+		var userKeys = [];
+		User.all().then(users => {
+			for (let i = 0; i < users.length; i++) {
+				userKeys.push(users[i].dataValues.id);
+			}
+			Meeting.create({
+				title: faker.lorem.slug(),
+				user_created_id: userKeys[Math.floor(Math.random() * userKeys.length)]
+			});
+		})
+	}
+	
+});
+
+router.get('/users', (req, res) => {
+    for (let i = 0; i < maxRecord; i++) {
+	    User.create({
+	        name: faker.fake("{{name.lastName}} {{name.firstName}}"),
+	        email: faker.internet.email(),
+	        password: '1234'
+	    });
+	}
+});
+
+router.get('/meetings', (req, res) => {
+    for (let i = 0; i < maxRecord; i++) {
+		var userKeys = [];
+		User.all().then(users => {
+			for (let i = 0; i < users.length; i++) {
+				userKeys.push(users[i].dataValues.id);
+			}
+			Meeting.create({
+				title: faker.lorem.slug(),
+				user_created_id: userKeys[Math.floor(Math.random() * userKeys.length)]
+			});
+		})
+	}	
 });
 
 module.exports = router;
