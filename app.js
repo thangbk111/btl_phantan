08cf2e721fa50127777926d233d2f613fabdfea0
thangbk111 <nguyenvanthang2911@gmail.com>
2018-11-24 15:35:18 +0700

    test realtime

--- app.js
+++ app.js
@@ -6,102 +6,10 @@ var users = require('./routes/users');
 var meetings = require('./routes/meetings');
 var roles = require('./routes/roles');
 var subContents = require('./routes/sub_contents');
-var SubContent = require('./models/sub_content');
 var isAuthenticated = require('./middleware/authenticate');
 var Authorization = require('./middleware/authorization');
-const TYPE_FILE1 = 0; // {'author', 'start_time', 'end_time'}
-const TYPE_FILE2 = 1; // {start_time', 'end_time', 'content'}
-const TYPE_FILE3 = 2; // {'author', 'start_time', 'end_time', 'content'}
-const CONFLICT = 1;
-const NO_CONFLICT = 0;
-const FIXED_CONFLICT = 2;
-const FULL = 1;
-const MISSING = 0;
 
 const app = express();
-var io = require('./sockets/socket_server').createSocket();
-
-/*
-Client Emit ===> 
-{
-    "user_id"
-    "meeting_id"
-    "subcontent": {
-        "id"
-        "author"
-        "content"
-        "start_time"
-        "end_time"
-    }
-} 
-*/
-io.on('connection', (socket) => {
-    // Update and Add Channel
-    socket.on('edit_subcontent', function(data) {
-        if(!checkRole(Authorization.getUserRole(data.user_id, data.meeting_id))) {
-            io.emit('edit_subcontent', { 'status': false, 'data': 'user has not access to edit'});
-        }else{
-            if (data.subcontent.id === 0 ){
-                var {error} = validateTypeFile3(data.subcontent);
-                if (error) {
-                    io.emit('edit_subcontent', { 'status': false, 'data': error});
-                } else {
-                    SubContent.findOne({
-                        where: {
-                            'start_time': data.subcontent.start_time,
-                            'end_time': data.subcontent.end_time,
-                            'author': data.subcontent.author,
-                            'is_full': FULL
-                        }
-                    }).then(subcontent => {
-                        if (!subcontent) {
-                            SubContent.create({
-                                author: data.subcontent.author,
-                                content: data.subcontent.content,
-                                start_time: data.subcontent.start_time,
-                                end_time: data.subcontent.end_time,
-                                is_full: FULL,
-                                flag: FIXED_CONFLICT,
-                                user_id: data.user_id,
-                                meeting_id: data.meeting_id
-                            }).then(newSubContent => {
-                                io.emit('edit_subcontent', { 'status': true, 'data': newSubContent });
-                            });
-                        }
-                    });
-                }
-            } else {
-                SubContent.findById(data.subcontent.id).then(subContent => {
-                    if (!subContent) {
-                        io.emit('edit_subcontent', { 'status': false, 'data': 'This is no SubContent available to update'});
-                    } else {
-                        subContent.update({
-                            author: data.subcontent.author,
-                            content: data.subcontent.content,
-                            flag: FIXED_CONFLICT
-                        });
-                        io.emit('edit_subcontent', { 'status': true, 'data': subContent });
-                    }
-                });
-            }
-        }
-    });
-
-    // Delete Channel
-    socket.on('delete_subcontent', function(data) {
-        if(!checkRole(Authorization.getUserRole(data.user_id, data.meeting_id))) {
-            io.emit('edit_subcontent', { 'status': false, 'data': 'user has not access to edit'});
-        }else{
-            SubContent.findById(data.subcontent.id).then(subContent => {
-                if (!subContent) {
-                    io.emit('delete_subcontent', { 'status': false, 'data': 'This is no SubContent available to delete'});
-                }
-                subContent.destroy();
-                io.emit('delete_subcontent', { 'status': true, 'data': subContent });
-            });
-        }
-    });
-});
 
 //Configs
 app.set('port', 3000);
@@ -118,20 +26,3 @@ app.use('/api/sub_contents', isAuthenticated, subContents);
 app.use('/api/invite_meeting', isAuthenticated, roles);
 
 app.listen(app.get('port'), () => console.log(`Listening to port ${app.get('port')}`));
-
-function checkRole(role) {
-    if(role === Authorization.VIEWER) {
-        return 0;
-    }
-    return 1;
-}
-
-function validateTypeFile3(content) {
-    schema = Joi.object().keys({
-        author: Joi.string().required(),
-        content: Joi.string().required(),
-        start_time: Joi.date().iso().less(Joi.ref('end_time')).required(),
-        end_time: Joi.date().iso().greater(Joi.ref('start_time')).required()
-    });
-    return Joi.validate(content, schema);
-}
--- routes/sub_contents.js
+++ routes/sub_contents.js
@@ -16,8 +16,117 @@ const FIXED_CONFLICT = 2;
 const FULL = 1;
 const MISSING = 0;
 
+/*****************************SOCKET
+ * ***************************
+ * ***************************
+ *****************************/
+
+var io = require('../sockets/socket_server').createSocket();
+/*
+Client Emit ===> 
+{
+    "user_id"
+    "meeting_id"
+    "subcontent": {
+        "id"
+        "author"
+        "content"
+        "start_time"
+        "end_time"
+    }
+} 
+*/
+io.on('connection', (socket) => {
+    // Update and Add Channel
+    socket.on('edit_subcontent', function(data) {
+        if(!checkRole(Authorization.getUserRole(data.user_id, data.meeting_id))) {
+            io.emit('edit_subcontent', { 'status': false, 'data': 'user has not access to edit'});
+        }else{
+            if (data.subcontent.id === 0 ){
+                var {error} = validateTypeFile3(data.subcontent);
+                if (error) {
+                    io.emit('edit_subcontent', { 'status': false, 'data': error});
+                } else {
+                    SubContent.findOne({
+                        where: {
+                            'start_time': data.subcontent.start_time,
+                            'end_time': data.subcontent.end_time,
+                            'author': data.subcontent.author,
+                            'is_full': FULL
+                        }
+                    }).then(subcontent => {
+                        if (!subcontent) {
+                            SubContent.create({
+                                author: data.subcontent.author,
+                                content: data.subcontent.content,
+                                start_time: data.subcontent.start_time,
+                                end_time: data.subcontent.end_time,
+                                is_full: FULL,
+                                flag: FIXED_CONFLICT,
+                                user_id: data.user_id,
+                                meeting_id: data.meeting_id
+                            }).then(newSubContent => {
+                                io.emit('edit_subcontent', { 'status': true, 'data': newSubContent });
+                            });
+                        }
+                    });
+                }
+            } else {
+                SubContent.findById(data.subcontent.id).then(subContent => {
+                    if (!subContent) {
+                        io.emit('edit_subcontent', { 'status': false, 'data': 'This is no SubContent available to update'});
+                    } else {
+                        subContent.update({
+                            author: data.subcontent.author,
+                            content: data.subcontent.content,
+                            flag: FIXED_CONFLICT
+                        }).then(subcontentUpdated => {
+                            io.emit('edit_subcontent', { 'status': true, 'data': subContent });
+                        });
+                    }
+                });
+            }
+        }
+    });
+
+    // Delete Channel
+    socket.on('delete_subcontent', function(data) {
+        if(!checkRole(Authorization.getUserRole(data.user_id, data.meeting_id))) {
+            io.emit('edit_subcontent', { 'status': false, 'data': 'user has not access to edit'});
+        }else{
+            SubContent.findById(data.subcontent.id).then(subContent => {
+                if (!subContent) {
+                    io.emit('delete_subcontent', { 'status': false, 'data': 'This is no SubContent available to delete'});
+                }
+                subContent.destroy();
+                io.emit('delete_subcontent', { 'status': true, 'data': subContent });
+            });
+        }
+    });
+});
+
+function checkRole(role) {
+    if(role === Authorization.VIEWER) {
+        return 0;
+    }
+    return 1;
+}
+
+function validateTypeFile3(content) {
+    schema = Joi.object().keys({
+        author: Joi.string().required(),
+        content: Joi.string().required(),
+        start_time: Joi.date().iso().less(Joi.ref('end_time')).required(),
+        end_time: Joi.date().iso().greater(Joi.ref('start_time')).required()
+    });
+    return Joi.validate(content, schema);
+}
+
+/********************* REST API
+********************************* 
+***********************/
+
 router.get('/:meetingId', Authorization.isViewer, (req, res) => {
-    console.log('meetingnngngngngngn');
     SubContent.findAll({
         where: {
             meeting_id: req.params.meetingId
@@ -60,7 +169,8 @@ router.post('/:meetingId', Authorization.isEditerOrOwnerMeeting, (req, res) => {
                 }
             }).then(subcontents => {
                 if (subcontents.length === 0) {
-                    createNewSubContent(TYPE_FILE1, contents[i], NO_CONFLICT, MISSING);
+                    var newSubContent = createNewSubContent(TYPE_FILE1, contents[i], NO_CONFLICT, MISSING);
+                    io.emit('add_subcontent', newSubContent);
                 }
                 else if (subcontents.length === 1) {
                     //CASE: No Conflict. MISSING author ==> Update
@@ -68,6 +178,8 @@ router.post('/:meetingId', Authorization.isEditerOrOwnerMeeting, (req, res) => {
                         subcontents[0].update({
                             author: contents[i].author,
                             is_full: FULL
+                        }).then(subcontentUpdated => {
+                            io.emit('update_subcontent', subcontentUpdated);
                         });
                     }
                     //CASE: Conflict Author
@@ -75,8 +187,11 @@ router.post('/:meetingId', Authorization.isEditerOrOwnerMeeting, (req, res) => {
                         flagConfict = true;
                         subcontents[0].update({
                             flag: CONFLICT
+                        }).then(subcontentUpdated => {
+                            io.emit('update_subcontent', subcontentUpdated);
                         });
-                        createNewSubContent(TYPE_FILE1, contents[i], CONFLICT, MISSING);
+                        var newSubContent = createNewSubContent(TYPE_FILE1, contents[i], CONFLICT, MISSING);
+                        io.emit('add_subcontent', newSubContent);
                     }
                 }
                 else {
@@ -87,7 +202,8 @@ router.post('/:meetingId', Authorization.isEditerOrOwnerMeeting, (req, res) => {
                     //Conflict Author
                     if (authors.indexOf(contents[i].author) === -1) {
                         flagConfict = true;
-                        createNewSubContent(TYPE_FILE1, contents[i], CONFLICT, MISSING);
+                        var newSubContent = createNewSubContent(TYPE_FILE1, contents[i], CONFLICT, MISSING);
+                        io.emit('add_subcontent', newSubContent);
                     }
                 }
             });
@@ -119,16 +235,20 @@ router.post('/:meetingId', Authorization.isEditerOrOwnerMeeting, (req, res) => {
                 }
             }).then(subcontent => {
                 if (!subcontent) {
-                    createNewSubContent(TYPE_FILE2, contents[i], NO_CONFLICT, MISSING);
+                    var newSubContent = createNewSubContent(TYPE_FILE2, contents[i], NO_CONFLICT, MISSING);
+                    io.emit('add_subcontent', newSubContent);
                 } else {
                     //CASE: MISSING CONTENT ===> Update
                     if (subcontent.is_full === MISSING && subcontent.content === null && subcontent.flag === NO_CONFLICT) {
                         subcontent.update({
                             content: contents[i].content,
                             is_full: FULL
+                        }).then(subcontentUpdated => {
+                            io.emit('update_subcontent', subcontentUpdated);
                         });
                     } else {
-                        createNewSubContent(TYPE_FILE2, contents[i], NO_CONFLICT, MISSING);
+                        var newSubContent = createNewSubContent(TYPE_FILE2, contents[i], NO_CONFLICT, MISSING);
+                        io.emit('add_subcontent', newSubContent);
                     }
                 }
             });
@@ -136,29 +256,30 @@ router.post('/:meetingId', Authorization.isEditerOrOwnerMeeting, (req, res) => {
         return res.json({'status': true, 'data': 'Data was added successfull'});
     }
     //TYPE 3
-    if (req.body.type === TYPE_FILE3) {
-        var errors = validateContentFile(contents, TYPE_FILE3);
-        if (errors.length !== 0) {
-            return res.json({'status': false, 'data': errors});
-        }
-        for (let i = 0; i < contents.length; i++) {
-            contents[i].userId = userId;
-            contents[i].meetingId = meetingId;
-            SubContent.findOne({
-                where: {
-                    'start_time': contents[i].start_time,
-                    'end_time': contents[i].end_time,
-                    'author': contents[i].author,
-                    'is_full': FULL
-                }
-            }).then(subcontent => {
-                if (!subcontent) {
-                    createNewSubContent(TYPE_FILE3, contents[i], FIXED_CONFLICT, FULL);
-                }
-            });
-        }
-        return res.json({'status': true, 'data': 'Data was added successfull'});
-    }
+    // if (req.body.type === TYPE_FILE3) {
+    //     var errors = validateContentFile(contents, TYPE_FILE3);
+    //     if (errors.length !== 0) {
+    //         return res.json({'status': false, 'data': errors});
+    //     }
+    //     for (let i = 0; i < contents.length; i++) {
+    //         contents[i].userId = userId;
+    //         contents[i].meetingId = meetingId;
+    //         SubContent.findOne({
+    //             where: {
+    //                 'start_time': contents[i].start_time,
+    //                 'end_time': contents[i].end_time,
+    //                 'author': contents[i].author,
+    //                 'is_full': FULL
+    //             }
+    //         }).then(subcontent => {
+    //             if (!subcontent) {
+    //                 var newSubContent = createNewSubContent(TYPE_FILE3, contents[i], FIXED_CONFLICT, FULL);
+    //                 io.emit('add_subcontent', newSubContent);
+    //             }
+    //         });
+    //     }
+    //     return res.json({'status': true, 'data': 'Data was added successfull'});
+    // }
 });
 
 //Update all content
@@ -241,6 +362,8 @@ function createNewSubContent(typeFile, _content, flag, isFull) {
             flag: flag,
             user_id: _content.userId,
             meeting_id: _content.meetingId
+        }).then(subcontent => {
+            return subcontent;
         });
     }
     if (typeFile === TYPE_FILE2) {
@@ -252,6 +375,8 @@ function createNewSubContent(typeFile, _content, flag, isFull) {
             flag: flag,
             user_id: _content.userId,
             meeting_id: _content.meetingId
+        }).then(subcontent => {
+            return subcontent;
         });
     }
     if (typeFile === TYPE_FILE3) {
@@ -264,6 +389,8 @@ function createNewSubContent(typeFile, _content, flag, isFull) {
             flag: flag,
             user_id: _content.userId,
             meeting_id: _content.meetingId
+        }).then(subcontent => {
+            return subcontent;
         });
     }
 }
