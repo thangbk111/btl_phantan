a0fce0ffb0480c5729b77d615e831f207fc14d4e
thangbk111 <nguyenvanthang2911@gmail.com>
2018-11-24 14:13:45 +0700

    test sockettt

---
+++ sockets/socket_server.js
@@ -0,0 +1,7 @@
+function createSocket() {
+    var socketServer = require('http').createServer();
+    var io = require('socket.io')(socketServer);
+    socketServer.listen(8080, () => console.log('Socket Server listening port 8080'));
+    return io;
+}
+
